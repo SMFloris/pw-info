c1e36a42d01797d543620d39192ac21338e4c392
flowriser <floris.sm@gmail.com>
2022-06-12 00:14:50 +0300

    docs: add documentation

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# pw-info
-Small utility written in Go that can find and filter pipewire objects based on their properties.
+# Description
+Small utility written in Go that can find and filter pipewire objects based on their properties.  
+Uses `pw-cli dump` to get the data. 
+
+This small utility has been motivated by the fact that pipewire does not have a nicer way of finding nodes other than using their ids.
+
+Neither it or wireplumber makes it easy through cli to find the node's property.
+
+# Usage
+
+Get node based on property value:
+```
+$ pw-info find-node node.name alsa_output.pci-0000_31_00.4.analog-stereo
+id: 1
+type: PipeWire:Interface:Node/3
+```
+
+Get node property based on id:
+```
+$ pw-info find-node-property 1 node.description
+node.description: Headphones
+```
+
+# Use-cases
+
+For example, if you know the alsa node name; and you use wireplumber to set its custom description for your riced desktop and would like that custom name appearing instead of default name.
+
+```
+# alsa-headphones.lua
+rule = {
+    matches = {
+      {
+        { "node.name", "equals", "alsa_output.pci-0000_31_00.4.analog-stereo" },
+      },
+    },
+    apply_properties = {
+      ["node.description"] = "Headphones",
+    },
+  }
+  
+  table.insert(alsa_monitor.rules,rule)
+```
+
+Now, getting the name of the default sink works kinda like this with this utility:
+
+```sh
+# this will get you something like: alsa_output.pci-0000_31_00.4.analog-stereo
+DEFAULT_SINK_ID=$(pactl get-default-sink)
+
+# this will get you the volume in percentage
+VOLUME=$(pamixer --get-volume-human | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1)
+
+# this will get you the description you put in wireplumber: Headphones
+CUSTOM_SINK_NAME=$(pw-info find-node -s node.name alsa_output.pci-0000_31_00.4.analog-stereo | head -1 | xargs -I{} pw-info find-node-property -s {} node.description)
+
+# we add everything up: 20% Headphone
+echo "${VOLUME}% ${CUSTOM_SINK_NAME}"
+```
--- main.go
+++ main.go
@@ -24,8 +24,9 @@ func main() {
 
 	app.Command("find-node-property", "gets the value of a specific property from a node", func(cmd *cli.Cmd) {
 		var (
-			id   = cmd.IntArg("ID", 0, "id of the node")
-			name = cmd.StringArg("NAME", "", "property name to return")
+			id    = cmd.IntArg("ID", 0, "id of the node")
+			name  = cmd.StringArg("NAME", "", "property name to return")
+			short = cmd.BoolOpt("s short", false, "do not output key names, only values")
 		)
 
 		cmd.Action = func() {
@@ -34,47 +35,55 @@ func main() {
 				if err != nil {
 					log.Fatal(err)
 				}
-				PrintProperties(properties)
+				PrintProperties(properties, *short)
 				return
 			}
 			property, err := pipewire.GetProperty(nodes, *id, *name)
 			if err != nil {
 				log.Fatal(err)
 			}
-			PrintProperty(property)
+			PrintProperty(property, *short)
 		}
 	})
 
-	app.Command("find-node", "Returns a node id based on a property value", func(cmd *cli.Cmd) {
+	app.Command("find-node", "Returns a node based on a property value", func(cmd *cli.Cmd) {
 		var (
 			name  = cmd.StringArg("NAME", "", "name of the property")
 			value = cmd.StringArg("VALUE", "", "value of the property")
+			short = cmd.BoolOpt("s short", false, "do not output key names, only values")
 		)
 
 		cmd.Action = func() {
-			fmt.Println(*name)
 			node, err := pipewire.FindNode(nodes, *name, *value)
 			if err != nil {
 				log.Fatal(err)
 			}
-			PrintNode(node)
+			PrintNode(node, *short)
 		}
 	})
 
 	app.Run(os.Args)
 }
 
-func PrintNode(node pipewire.PipewireNode) {
-	fmt.Printf("%s: %d\n", "id", node.Id)
-	fmt.Printf("%s: %s\n", "type", node.Type)
+func PrintNode(node pipewire.PipewireNode, short bool) {
+	fmt.Printf("%s%d\n", getShortKey("id: ", short), node.Id)
+	fmt.Printf("%s%s\n", getShortKey("type: ", short), node.Type)
 }
 
-func PrintProperties(properties []pipewire.PipewireNodeProperty) {
+func PrintProperties(properties []pipewire.PipewireNodeProperty, short bool) {
 	for _, property := range properties {
-		PrintProperty(property)
+		PrintProperty(property, short)
 	}
 }
 
-func PrintProperty(property pipewire.PipewireNodeProperty) {
-	fmt.Printf("%s: %s\n", property.Key, property.Value)
+func PrintProperty(property pipewire.PipewireNodeProperty, short bool) {
+	fmt.Printf("%s%s\n", getShortKey(property.Key, short), property.Value)
+}
+
+func getShortKey(key string, short bool) string {
+	if short {
+		return ""
+	}
+
+	return key + ": "
 }
